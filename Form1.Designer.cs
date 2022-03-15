018301b64b81f7be309fafe5744d7c2cdf33c673
rumen1910 <r191001@abv.bg>
2022-03-15 15:36:31 +0200

    DataSaver

---
+++ Form1.Designer.cs
@@ -0,0 +1,322 @@
+﻿namespace DataSave
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.NameBox = new System.Windows.Forms.TextBox();
+            this.AgeBox = new System.Windows.Forms.TextBox();
+            this.CityBox = new System.Windows.Forms.TextBox();
+            this.SalaryBox = new System.Windows.Forms.TextBox();
+            this.AddressBox = new System.Windows.Forms.TextBox();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.DeleteButton = new System.Windows.Forms.Button();
+            this.CancelButton = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.employeeNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.employeeAgeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.employeeCityDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.employeeSalaryDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.employeeAddressDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.employeeTableBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.employeeDBDataSet1 = new DataSave.EmployeeDBDataSet1();
+            this.employeeDBDataSet = new DataSave.EmployeeDBDataSet();
+            this.employeeTableBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.employeeTableTableAdapter = new DataSave.EmployeeDBDataSetTableAdapters.EmployeeTableTableAdapter();
+            this.employeeTableTableAdapter1 = new DataSave.EmployeeDBDataSet1TableAdapters.EmployeeTableTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.employeeTableBindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.employeeDBDataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.employeeDBDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.employeeTableBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(27, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Employee Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(27, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(124, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Employee Age";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(27, 157);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(122, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Employee City";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(27, 219);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(142, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Employee Salary";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(27, 283);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(158, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Employee Address";
+            // 
+            // NameBox
+            // 
+            this.NameBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.NameBox.Location = new System.Drawing.Point(171, 39);
+            this.NameBox.Name = "NameBox";
+            this.NameBox.Size = new System.Drawing.Size(185, 24);
+            this.NameBox.TabIndex = 5;
+            // 
+            // AgeBox
+            // 
+            this.AgeBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.AgeBox.Location = new System.Drawing.Point(171, 95);
+            this.AgeBox.Name = "AgeBox";
+            this.AgeBox.Size = new System.Drawing.Size(185, 24);
+            this.AgeBox.TabIndex = 6;
+            // 
+            // CityBox
+            // 
+            this.CityBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.CityBox.Location = new System.Drawing.Point(171, 157);
+            this.CityBox.Name = "CityBox";
+            this.CityBox.Size = new System.Drawing.Size(185, 24);
+            this.CityBox.TabIndex = 7;
+            // 
+            // SalaryBox
+            // 
+            this.SalaryBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.SalaryBox.Location = new System.Drawing.Point(175, 219);
+            this.SalaryBox.Name = "SalaryBox";
+            this.SalaryBox.Size = new System.Drawing.Size(181, 24);
+            this.SalaryBox.TabIndex = 8;
+            // 
+            // AddressBox
+            // 
+            this.AddressBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.AddressBox.Location = new System.Drawing.Point(191, 279);
+            this.AddressBox.Multiline = true;
+            this.AddressBox.Name = "AddressBox";
+            this.AddressBox.Size = new System.Drawing.Size(165, 83);
+            this.AddressBox.TabIndex = 9;
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Bold);
+            this.SaveButton.Location = new System.Drawing.Point(47, 446);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(170, 44);
+            this.SaveButton.TabIndex = 10;
+            this.SaveButton.Text = "Save";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // DeleteButton
+            // 
+            this.DeleteButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Bold);
+            this.DeleteButton.Location = new System.Drawing.Point(269, 446);
+            this.DeleteButton.Name = "DeleteButton";
+            this.DeleteButton.Size = new System.Drawing.Size(170, 44);
+            this.DeleteButton.TabIndex = 11;
+            this.DeleteButton.Text = "Delete";
+            this.DeleteButton.UseVisualStyleBackColor = true;
+            // 
+            // CancelButton
+            // 
+            this.CancelButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Bold);
+            this.CancelButton.Location = new System.Drawing.Point(487, 446);
+            this.CancelButton.Name = "CancelButton";
+            this.CancelButton.Size = new System.Drawing.Size(170, 44);
+            this.CancelButton.TabIndex = 12;
+            this.CancelButton.Text = "Cancel";
+            this.CancelButton.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.employeeNameDataGridViewTextBoxColumn,
+            this.employeeAgeDataGridViewTextBoxColumn,
+            this.employeeCityDataGridViewTextBoxColumn,
+            this.employeeSalaryDataGridViewTextBoxColumn,
+            this.employeeAddressDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.employeeTableBindingSource1;
+            this.dataGridView1.Location = new System.Drawing.Point(382, 38);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(544, 323);
+            this.dataGridView1.TabIndex = 13;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // employeeNameDataGridViewTextBoxColumn
+            // 
+            this.employeeNameDataGridViewTextBoxColumn.DataPropertyName = "EmployeeName";
+            this.employeeNameDataGridViewTextBoxColumn.HeaderText = "EmployeeName";
+            this.employeeNameDataGridViewTextBoxColumn.Name = "employeeNameDataGridViewTextBoxColumn";
+            // 
+            // employeeAgeDataGridViewTextBoxColumn
+            // 
+            this.employeeAgeDataGridViewTextBoxColumn.DataPropertyName = "EmployeeAge";
+            this.employeeAgeDataGridViewTextBoxColumn.HeaderText = "EmployeeAge";
+            this.employeeAgeDataGridViewTextBoxColumn.Name = "employeeAgeDataGridViewTextBoxColumn";
+            // 
+            // employeeCityDataGridViewTextBoxColumn
+            // 
+            this.employeeCityDataGridViewTextBoxColumn.DataPropertyName = "EmployeeCity";
+            this.employeeCityDataGridViewTextBoxColumn.HeaderText = "EmployeeCity";
+            this.employeeCityDataGridViewTextBoxColumn.Name = "employeeCityDataGridViewTextBoxColumn";
+            // 
+            // employeeSalaryDataGridViewTextBoxColumn
+            // 
+            this.employeeSalaryDataGridViewTextBoxColumn.DataPropertyName = "EmployeeSalary";
+            this.employeeSalaryDataGridViewTextBoxColumn.HeaderText = "EmployeeSalary";
+            this.employeeSalaryDataGridViewTextBoxColumn.Name = "employeeSalaryDataGridViewTextBoxColumn";
+            // 
+            // employeeAddressDataGridViewTextBoxColumn
+            // 
+            this.employeeAddressDataGridViewTextBoxColumn.DataPropertyName = "EmployeeAddress";
+            this.employeeAddressDataGridViewTextBoxColumn.HeaderText = "EmployeeAddress";
+            this.employeeAddressDataGridViewTextBoxColumn.Name = "employeeAddressDataGridViewTextBoxColumn";
+            // 
+            // employeeTableBindingSource1
+            // 
+            this.employeeTableBindingSource1.DataMember = "EmployeeTable";
+            this.employeeTableBindingSource1.DataSource = this.employeeDBDataSet1;
+            // 
+            // employeeDBDataSet1
+            // 
+            this.employeeDBDataSet1.DataSetName = "EmployeeDBDataSet1";
+            this.employeeDBDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // employeeDBDataSet
+            // 
+            this.employeeDBDataSet.DataSetName = "EmployeeDBDataSet";
+            this.employeeDBDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // employeeTableBindingSource
+            // 
+            this.employeeTableBindingSource.DataMember = "EmployeeTable";
+            this.employeeTableBindingSource.DataSource = this.employeeDBDataSet;
+            // 
+            // employeeTableTableAdapter
+            // 
+            this.employeeTableTableAdapter.ClearBeforeFill = true;
+            // 
+            // employeeTableTableAdapter1
+            // 
+            this.employeeTableTableAdapter1.ClearBeforeFill = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(958, 504);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.CancelButton);
+            this.Controls.Add(this.DeleteButton);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.AddressBox);
+            this.Controls.Add(this.SalaryBox);
+            this.Controls.Add(this.CityBox);
+            this.Controls.Add(this.AgeBox);
+            this.Controls.Add(this.NameBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.employeeTableBindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.employeeDBDataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.employeeDBDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.employeeTableBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox NameBox;
+        private System.Windows.Forms.TextBox AgeBox;
+        private System.Windows.Forms.TextBox CityBox;
+        private System.Windows.Forms.TextBox SalaryBox;
+        private System.Windows.Forms.TextBox AddressBox;
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.Button DeleteButton;
+        private System.Windows.Forms.Button CancelButton;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private EmployeeDBDataSet employeeDBDataSet;
+        private System.Windows.Forms.BindingSource employeeTableBindingSource;
+        private EmployeeDBDataSetTableAdapters.EmployeeTableTableAdapter employeeTableTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn employeeNameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn employeeAgeDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn employeeCityDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn employeeSalaryDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn employeeAddressDataGridViewTextBoxColumn;
+        private EmployeeDBDataSet1 employeeDBDataSet1;
+        private System.Windows.Forms.BindingSource employeeTableBindingSource1;
+        private EmployeeDBDataSet1TableAdapters.EmployeeTableTableAdapter employeeTableTableAdapter1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace DataSave
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+
+            SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-QSQGOK5;Initial Catalog=EmployeeDB;Integrated Security=True");
+            string InsertQuey = "Insert into EmployeeTable(EmployeeName,EmployeeAge,EmployeeCity,EmployeeSalary,EmployeeAddress)Values('" + NameBox.Text + "','" + AgeBox.Text + "','" + CityBox.Text + "','" + SalaryBox.Text + "','" + AddressBox.Text + "')";
+            con.Open();
+            SqlCommand cmd = new SqlCommand(InsertQuey, con);
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'employeeDBDataSet1.EmployeeTable' table. You can move, or remove it, as needed.
+            this.employeeTableTableAdapter1.Fill(this.employeeDBDataSet1.EmployeeTable);
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
